9fdeea0fda59ef11db61d17573ae0d207fc97f3e
yash0212 <yashlotan7@gmail.com>
2019-02-23 02:46:45 +0530

    Directory Restructure

---
+++ models/User.js
@@ -0,0 +1,96 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  // name: String,
+  email: { type: String, required: true, unique: true },
+  // username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phoneno: String,
+  // admin: Boolean,
+  // location: String,
+  // meta: {
+  // age: Number,
+  // website: String
+  // },
+  created_at: Date,
+  updated_at: Date
+});
+
+var userModel = mongoose.model("User", userSchema);
+
+class User {
+  // var email;
+  // var password;
+  // var name;
+  // var address;
+  // var phoneno;
+
+  constructor(email, password, phoneno) {
+    this.email = email;
+    this.password = password;
+    this.phoneno = phoneno;
+  }
+
+  insertInDb() {
+    var u = new userModel({
+      email: this.email,
+      password: this.password,
+      phoneno: this.phoneno
+    });
+    u.save(err => {
+      if (err) {
+        throw err;
+      }
+      //   console.log("User successfully saved.");
+    });
+  }
+
+  findUser() {
+    var email = this.email;
+    var password = this.password;
+    return new Promise((resolve, reject) => {
+      userModel.findOne({ email: email, password: password }, (err, user) => {
+        if (err) {
+          throw err;
+        }
+        resolve(user);
+      });
+    });
+  }
+
+  findAll() {
+    return new Promise(function(resolve, reject) {
+      userModel.find({}, function(err, users) {
+        if (err) throw err;
+
+        // object of all the users
+        resolve(users);
+      });
+    });
+  }
+  checkDuplicate() {
+    var email = this.email;
+    var phoneno = this.phoneno;
+
+    return new Promise(function(resolve, reject) {
+      userModel.find(
+        {
+          email: email,
+          phoneno: phoneno
+        },
+        function(err, users) {
+          if (users.length !== 0) {
+            //Duplicate User
+            resolve(1);
+          } else {
+            //Not duplicate
+            resolve(0);
+          }
+        }
+      );
+    });
+  }
+}
+
+module.exports = User;
---
+++ routes/index.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../models/User");
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  // res.render("index", { title: "Express", layout: "layout" });
+  var loggedIn = req.session.userId ? 1 : 0;
+  res.render("index", { title: "Pizzaero", loggedIn: loggedIn });
+});
+
+router.get("/register", function(req, res, next) {
+  res.render("register", { title: "Register" });
+});
+
+router.post("/register", async function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var phoneno = req.body.phoneno;
+
+  var user = new User(email, password, phoneno);
+  //check if user already exists
+  var isDuplicate = await user.checkDuplicate();
+  if (isDuplicate) {
+    //Duplicate User, redirect back to registration page
+    res.render("register", { title: "Register", error: "Duplicate User" });
+    // res.redirect("/register?error=" + "Duplicate User");
+  } else {
+    //User not duplicate so enter user details in database
+    user.insertInDb();
+    res.redirect("/");
+  }
+});
+
+// router.get("/login", function(req, res, next) {
+//   res.render("login", { title: "Login" });
+// });
+
+router.post("/login", async function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var phoneno = req.body.phoneno;
+
+  var userMod = new User(email, password, phoneno);
+  var user = await userMod.findUser();
+  if (user == null) {
+    // res.render("login", {
+    //   title: "Login",
+    //   error: "Wrong Credentials"
+    // });
+    console.log("wrong credentials");
+    res.render("index", {
+      title: "Pizzaero",
+      loggedIn: 0,
+      error: "Wrong Credentials"
+    });
+  } else {
+    console.log("Successfull login");
+    req.session.userId = user._id;
+    res.redirect("/");
+  }
+});
+
+router.get("/logout", function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect("/");
+      }
+    });
+  }
+});
+// router.get("/allusers", async function(req, res, next) {
+//   var user = new User();
+//   var users = await user.findAll();
+//   console.log(users);
+//   res.render("allusers", { data: users });
+// });
+module.exports = router;
