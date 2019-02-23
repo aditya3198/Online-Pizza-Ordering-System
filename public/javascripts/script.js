f8d024c3c79e6223ed152c6ca5497477ccdac540
cout3 <sanket.singh45@gmail.com>
2019-02-23 22:22:33 +0530

    added register page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,19 @@
 // script.js
 
 $(document).ready(function() {
-    
-    $('.dropdown').hover(function(){ 
-        $('.dropdown-toggle', this).trigger('click'); 
-      });
+  $(".dropdown").hover(function() {
+    $(".dropdown-toggle", this).trigger("click");
+  });
 });
-    
-
-
-
-
-
-
-
-
+function check() {
+  if (
+    document.getElementById("password").value ==
+    document.getElementById("confirm_password").value
+  ) {
+    document.getElementById("message").style.color = "green";
+    document.getElementById("message").innerHTML = "matching";
+  } else {
+    document.getElementById("message").style.color = "red";
+    document.getElementById("message").innerHTML = "not matching";
+  }
+}
